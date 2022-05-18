499e5ba99bac2e39c5076012d9ebe2414fa078c7
Simontaurus <simon.stier@gmx.de>
2022-05-18 07:41:44 +0200

    init pageimporter hook

--- includes/SemanticProperties.php
+++ includes/SemanticProperties.php
@@ -9,5 +9,21 @@ class SemanticProperties {
 		return true;
 
 	}
+    
+	public static function onPageImporterRegisterPageLists( array &$pageLists ) {
+
+		$pageLists['SemanticProperties'] = [
+			// list of pages to create and the corresponding files to use as content
+			"pages" => [
+			],
+            
+			// base dir for imported pages
+			"root" => dirname(__DIR__) . '/importPages',
+
+			// edit summary used when PageImporter edits pages
+			"comment" => "Updated with content from Extension:SemanticProperties version 0.0.1"
+		];
+
+	}
 
 }
